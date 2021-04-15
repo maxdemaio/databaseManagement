3edd356a8768e77a3a3297164b53144e09a1b3c4
Maxwell DeMaio <maxdman321@gmail.com>
2021-04-15 14:06:50 -0400

    Finished up to problem 4 of module 4

--- module4PracticeProblems.sql
+++ module4PracticeProblems.sql
@@ -10,4 +10,17 @@ FROM Customer
 WHERE state = "CO";
 
 -- Problem 3
+SELECT *
+FROM EventRequest
+WHERE estcost > 4000
+ORDER BY dateheld;
 
+-- Problem 4
+SELECT eventno, dateheld, estaudience, status
+FROM EventRequest
+WHERE estaudience > 9000 OR (status = "pending" AND estaudience > 7000);
+
+-- Problem 5
+-- Join from customer and event request table
+-- we need to grab the customer names using the custno
+SELECT eventno, dateheld, custno
