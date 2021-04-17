5bb3ea78460b01ffef5a224a71a8c6ea67357de7
Maxwell DeMaio <maxdman321@gmail.com>
2021-04-17 11:38:01 -0400

    Finished through problem 6 of extra practice problems

--- module4ExtraPracticeProblems.sql
+++ module4ExtraPracticeProblems.sql
@@ -18,5 +18,17 @@ ORDER BY ProdMfg;
 -- Problem 4
 SELECT CustFirstName, CustLastName, CustBal, CustState
 FROM Customer
-WHERE CustCity = "Denver"
-	AND CustBal > 150;
+WHERE (CustCity = "Denver" AND CustBal > 150) OR (CustCity = "Seattle" AND CustBal > 300);
+
+-- Problem 5
+-- Cross product style join
+SELECT OrdNo, OrdDate, Ordertbl.Custno, CustFirstName
+FROM OrderTbl, Customer
+WHERE CustState = "CO"
+	AND OrderTbl.CustNo = Customer.CustNo;
+    
+-- Problem 6
+SELECT CustNo, AVG(CustBal) AS AvgCustBal, CustCity, CustState
+  FROM Customer
+  WHERE CustState = "WA"
+  GROUP BY CustCity;
