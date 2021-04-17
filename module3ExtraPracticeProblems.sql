476a7bb78ca7aa9b1bfbb2f337af086e5e2ffd05
Maxwell DeMaio <maxdman321@gmail.com>
2021-04-16 21:56:20 -0400

    Completed module 4 practice problems and started extra problems

---
+++ module3ExtraPracticeProblems.sql
@@ -0,0 +1,34 @@
+-- CREATE DATABASE Orders;
+USE Orders;
+
+CREATE TABLE Customer ( 
+	CustNo 	CHAR(8),
+	CustFirstName	VARCHAR(20) NOT NULL,
+	CustLastName	VARCHAR(30) NOT NULL,
+	CustCity		VARCHAR(30),
+	CustState	CHAR(2),
+	CustZip	CHAR(10),
+	CustBal		DECIMAL(12,2),
+	CONSTRAINT PKCustomer PRIMARY KEY (CustNo)  
+ );
+
+CREATE TABLE Employee( 
+	EmpNo 	 CHAR(8),
+	EmpFirstName VARCHAR(20) NOT NULL,
+	EmpLastName VARCHAR(30) NOT NULL,
+	EmpPhone		 CHAR(15),
+	EmpEMail		 VARCHAR(50) NOT NULL UNIQUE,
+	CONSTRAINT PKEmployee PRIMARY KEY (EmpNo),
+	CONSTRAINT UniqueEMail UNIQUE (EmpEMail) 
+);
+
+CREATE TABLE OrderTbl ( 
+	OrdNo 	CHAR(8),
+    OrdDate	DATE NOT NULL,
+    CustNo		CHAR(8) NOT NULL,
+    EmpNo	CHAR(8),
+	CONSTRAINT PKOrderTbl PRIMARY KEY (OrdNo) ,
+	CONSTRAINT FKCustNo FOREIGN KEY (CustNo) REFERENCES Customer (CustNo),
+	CONSTRAINT FKEmpNo FOREIGN KEY (EmpNo) REFERENCES Employee (EmpNo)
+);
+
---
+++ module4ExtraPracticeProblems.sql
@@ -0,0 +1,7 @@
+USE Orders;
+
+-- Problem 1
+SELECT CustFirstName, CustLastName, CustBal
+FROM Customer;
+
+-- Problem 2
