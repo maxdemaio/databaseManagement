19008b9c831cbfd093945443ab648957b8bcc3bd
Maxwell DeMaio <maxdman321@gmail.com>
2021-04-16 22:25:12 -0400

    Finished extra problem 4

--- module4ExtraPracticeProblems.sql
+++ module4ExtraPracticeProblems.sql
@@ -5,3 +5,18 @@ SELECT CustFirstName, CustLastName, CustBal
 FROM Customer;
 
 -- Problem 2
+SELECT CustFirstName, CustLastName, CustBal, CustState
+FROM Customer
+WHERE CustState = "CO";
+
+-- Problem 3
+SELECT *
+FROM Product
+WHERE ProdPrice > 50
+ORDER BY ProdMfg;
+
+-- Problem 4
+SELECT CustFirstName, CustLastName, CustBal, CustState
+FROM Customer
+WHERE CustCity = "Denver"
+	AND CustBal > 150;
