419aad22803042b597afd87bb59753cb94204afb
Maxwell DeMaio <maxdman321@gmail.com>
2021-04-18 18:10:41 -0400

    Completed problem 4 of module4 assignment

--- module4assignment.sql
+++ module4assignment.sql
@@ -15,3 +15,9 @@ SELECT *
 FROM Resourcetable
 WHERE rate BETWEEN 	10 and 20
 ORDER BY rate;
+
+-- Problem 4
+SELECT *
+FROM Eventrequest
+WHERE dateauth BETWEEN CAST('2018-07-01' AS DATE) AND CAST('2018-07-31' AS DATE);
+
