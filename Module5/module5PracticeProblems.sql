3268606092a50a318cd61cffa58f83e9333822b7
Maxwell DeMaio <maxdman321@gmail.com>
2021-04-21 15:46:41 -0400

    Completed problem 1 of module 5

--- Module5/module5PracticeProblems.sql
+++ Module5/module5PracticeProblems.sql
@@ -1,4 +1,12 @@
 USE Intercollegiateathletic;
 
-SELECT *
-FROM Customer;
+-- Problem 1
+SELECT Eventno, Dateheld, Eventrequest.Custno, Eventrequest.Facno,
+	Customer.Custname, Facility.Facname, Customer.city
+FROM Eventrequest, Customer, Facility
+WHERE Eventrequest.Custno = Customer.Custno
+	AND Eventrequest.Facno = Facility.Facno
+    AND YEAR(dateheld)='2018'
+    AND Customer.city = "Boulder";
+    
+-- Problem 2
