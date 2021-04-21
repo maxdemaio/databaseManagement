a76d44dcad38d545cb55c2bde4db6805d215354b
Maxwell DeMaio <maxdman321@gmail.com>
2021-04-21 16:35:25 -0400

    Finished module 5 problem 2

--- Module5/module5PracticeProblems.sql
+++ Module5/module5PracticeProblems.sql
@@ -10,3 +10,11 @@ WHERE Eventrequest.Custno = Customer.Custno
     AND Customer.city = "Boulder";
     
 -- Problem 2
+SELECT Eventno, Dateheld, Eventrequest.Custno, Eventrequest.Facno,
+	Customer.Custname, Facility.Facname,
+    (Eventrequest.Estcost/Eventrequest.Estaudience) AS CostPerPerson
+FROM Eventrequest, Customer, Facility
+WHERE Eventrequest.Custno = Customer.Custno
+	AND EventRequest.FacNo = Facility.FacNo
+	AND EstCost/EstAudience < 0.2
+    AND YEAR(dateheld) = '2018';
