c93adb90014dc198cffca682be84f7237822b2e7
Maxwell DeMaio <maxdman321@gmail.com>
2021-04-20 20:20:05 -0400

    Completed lectures on multiple table joins / grouping operations

--- module5practice.sql
+++ module5practice.sql
@@ -1,15 +1,8 @@
 USE test;
 
-SELECT Offering.OfferNo, 
-        COUNT(*) AS NumStudents 
-  FROM Enrollment, Offering  
-  WHERE Offering.OfferNo = Enrollment.OfferNo 
-    AND OffYear = 2017
-  GROUP BY Offering.OfferNo;
-
--- Equivalent version using the join operator style
- SELECT Offering.OfferNo, 
-        COUNT(*) AS NumStudents 
-  FROM Enrollment INNER JOIN Offering ON Offering.OfferNo = Enrollment.OfferNo
-  WHERE OffYear = 2017
-  GROUP BY Offering.OfferNo;
+SELECT CourseNo, Enrollment.OfferNo, 
+       Count(*) AS NumStudents
+ FROM Offering, Enrollment
+ WHERE Offering.OfferNo = Enrollment.OfferNo
+   AND OffYear = 2017 AND OffTerm = 'SPRING' 
+ GROUP BY Enrollment.OfferNo, CourseNo;
