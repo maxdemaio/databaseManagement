6a5f858dd02bf7f5fbdde052590ea389180d97cb
Maxwell DeMaio <maxdman321@gmail.com>
2021-04-26 21:44:18 -0400

    Added problem descriptions to module5 practice problems

--- Module5/Module5Assignment.sql
+++ Module5/Module5Assignment.sql
@@ -1,5 +1,10 @@
 USE intercollegiateathletic;
 
 -- Problem 1
-SELECT eventrequest.eventno, dateheld
-	FROM eventrequest;
+-- For event requests, list the event number, event date (eventrequest.dateheld), 
+-- and count of the event plans.  Only include event requests in the 
+-- result if the event request has more than one related 
+-- event plan with a work date in December 2018.
+SELECT eventrequest.eventno, eventrequest.dateheld,
+		COUNT(eventplan.PlanNo) AS NUM_EVENT_PLANS
+	FROM eventrequest, eventplan;
--- Module5/module5PracticeProblems.sql
+++ Module5/module5PracticeProblems.sql
@@ -1,6 +1,10 @@
+-- MySQL responses
+
 USE Intercollegiateathletic;
 
 -- Problem 1
+-- List the event number, date held, customer number, customer name, 
+-- facility number, and facility name of 2018 events placed by Boulder customers.
 SELECT Eventno, Dateheld, Eventrequest.Custno, Eventrequest.Facno,
 	Customer.Custname, Facility.Facname, Customer.city
 FROM Eventrequest, Customer, Facility
@@ -10,6 +14,10 @@ WHERE Eventrequest.Custno = Customer.Custno
     AND Customer.city = "Boulder";
     
 -- Problem 2
+-- List the customer number, customer name, event number, date held, 
+-- facility number, facility name, and estimated audience 
+-- cost per person (EstCost / EstAudience) for events held on 2018, in 
+-- which the estimated cost per person is less than $0.2
 SELECT Eventno, Dateheld, Eventrequest.Custno, Eventrequest.Facno,
 	Customer.Custname, Facility.Facname,
     (Eventrequest.Estcost/Eventrequest.Estaudience) AS CostPerPerson
@@ -20,6 +28,9 @@ WHERE Eventrequest.Custno = Customer.Custno
     AND YEAR(dateheld) = '2018';
     
 -- Problem 3
+-- List the customer number, customer name, and total estimated 
+-- costs for Approved events. The total amount of events is the sum of the estimated cost 
+-- for each event. Group the results by customer number and customer name.
 SELECT Customer.CustNo, CustName, SUM(EstCost) AS TotEstCost
   FROM EventRequest, Customer
   WHERE Status = 'Approved'
@@ -31,10 +42,13 @@ FROM Customer
 WHERE Custno = "C999";
 
 -- Problem 4
+-- Insert yourself as a new row in the Customer table.
 INSERT INTO Customer (CustNo, CustName, Address, Internal, Contact, Phone, City, State, Zip)
 VALUES ("C999", "MMA", "Box 3445", "N", "Joe Rogan", 685699, "Las Vegas", "NV", 80977);
 
 -- Problem 5S
+-- Increase the rate by 10 percent of resource names equal to nurse. 
+-- In MySQL, you need to place the UPDATE statement between two SET statements.
 SET SQL_SAFE_UPDATES = 0;
 
 UPDATE Resourcetable
@@ -42,6 +56,7 @@ UPDATE Resourcetable
   WHERE ResName = 'nurse';
 
 -- Problem 6
+-- Delete the new row added to the Customer table.
 DELETE FROM Customer
   WHERE CustNo = 'C999';
 
