979c5f4af45bdd26007ea1ed387154e3c6568699
Maxwell DeMaio <maxdman321@gmail.com>
2021-04-26 23:22:14 -0400

    Completed problem 2 of module 5 assignment

--- Module5/Module5Assignment.sql
+++ Module5/Module5Assignment.sql
@@ -11,3 +11,16 @@ SELECT eventrequest.eventno, eventrequest.dateheld,
     WHERE eventrequest.eventno = eventplans.eventno
     GROUP BY EventRequest.EventNo, eventrequest.dateheld
     HAVING numEventPlans > 1;
+    
+-- Problem 2
+-- List the plan number, event number, work date, and 
+-- activity of event plans meeting the following two conditions: 
+-- (1) the work date is in December 2018 and (2) the event is held in the “Basketball arena”. 
+--  Your query must not use the facility number (“F101”) of the basketball arena in the WHERE clause. 
+--  Instead, you should use a condition on the FacName column for the value of “Basketball arena”.
+SELECT eventrequest.eventno, eventplans.planno, 
+		eventplans.workdate, eventplans.activity, facility.facname
+	FROM eventrequest, eventplans, facility
+    WHERE eventrequest.eventno = eventplans.eventno
+		AND eventrequest.facno = facility.facno
+        AND facname = "Basketball arena";
