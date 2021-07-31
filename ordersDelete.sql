c9e784b68f17bed0855e69cf9d8d87ca7f7baf05
Maxwell DeMaio <maxdman321@gmail.com>
2021-07-31 13:18:13 -0400

    updated for yt video

--- ordersDelete.sql
+++ ordersDelete.sql
@@ -5,16 +5,20 @@ SELECT * FROM customer;
 SELECT * FROM employee;
 SELECT * FROM ordertbl;
 
+-- Update customer first name
+-- UPDATE Customer
+--   SET CustFirstName = 'George'
+--   WHERE CustNo = 'C0954327';
+  
 -- Delete rows added
-SET SQL_SAFE_UPDATES = 0;
+-- SET SQL_SAFE_UPDATES = 1;
 
-DELETE FROM OrderTbl
-  WHERE OrdNo = 'O9919699';
-  
-DELETE FROM Employee
-  WHERE EmpNo = 'E8544399';
-  
-DELETE FROM Customer
-  WHERE CustNo = 'C0954327';
+-- DELETE FROM OrderTbl
+--   WHERE OrdNo = 'O9919699';
+--   
+-- DELETE FROM Employee
+--   WHERE EmpNo = 'E8544399';
+--   
+-- DELETE FROM Customer
+--   WHERE CustNo = 'C0954327';
 
-SET SQL_SAFE_UPDATES = 1;
